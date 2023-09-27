a32cf27d79127196d864562fec741c0011e6580c
vagnerasilva <vagnerasilva>
2023-09-27 19:51:38 -0300

    feat: ajustes no primeiro commit

---
+++ src/indexcssfiles.js
@@ -0,0 +1,8 @@
+require('./assets/css/bootstrap.min.css');
+import 'owl.carousel/dist/assets/owl.carousel.css';
+require('./assets/css/aos.css');
+require('./assets/css/slick.css');
+require('./assets/css/slick-theme.css');
+require('./assets/css/bootstrap-datepicker.min.css');
+require('./assets/css/magnific-popup.css');
+require('./assets/css/styles.css');
